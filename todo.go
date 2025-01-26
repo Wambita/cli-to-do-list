8fb62067e28c7359ab722fca5fbe58f120ff4e5c
shfana <wambitafana@gmail.com>
2025-01-26 17:35:40 +0300

    Feat: create a todo and todos struct

---
+++ todo.go
@@ -0,0 +1,15 @@
+package main
+
+import "time"
+
+type Todo struct {
+	Title       string
+	Completed   bool
+	CreatedAt   time.Time
+	CompletedAt *time.Time
+}
+
+type Todos []Todo
+
+func (todos *Todos) add(title string) {
+}
