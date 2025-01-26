27a6b2906184dc570c3cfbe4b8ec467ecdc88bc7
shfana <wambitafana@gmail.com>
2025-01-26 19:04:29 +0300

    Feat: create strings struct to  execute functions for the to do list,  a  constructor and a method to execute the flags

--- command.go
+++ command.go
@@ -1,6 +1,12 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 // commands
 type CmdFlags struct {
@@ -8,7 +14,7 @@ type CmdFlags struct {
 	Del    int
 	Edit   string
 	Toggle int
-	list   bool
+	List   bool
 }
 
 // initialze commnads
@@ -18,7 +24,7 @@ func NewCmdFlags() *CmdFlags {
 	flag.StringVar(&cf.Edit, "Edit", "", "Edit a to do by index and specify a new title id:new_title")
 	flag.IntVar(&cf.Del, "Del", -1, "Specify a to do by index to delete")
 	flag.IntVar(&cf.Toggle, "toggle", -1, "Specify a to do by index to toggle completed status")
-	flag.BoolVar(&cf.list, "list", false, "List all todos")
+	flag.BoolVar(&cf.list, "List", false, "List all todos")
 
 	//parse the arguments
 	flag.Parse()
@@ -31,10 +37,30 @@ func NewCmdFlags() *CmdFlags {
 //execute the flags
 func (cf *CmdFlags)  Execute ( todos *Todos) {
 	switch{
-	case cf.list:
+	case cf.List:
 		todos.print()
 	case cf.Add != "": 
 		todos.add(cf.Add)
-	case 		
+	case cf.Edit != "":
+		parts := strings.SplitN(cf.Edit, ":", 2)
+		if len(parts) != 2 {
+			fmt.Println("Error: invalid format for edit. Please use id:new_title")
+			os.Exit(1)
+		}
+		index, err := strconv.Atoi(parts[0])
+		if err != nil {
+			fmt.Println("Error: invalid index for edit")
+			os.Exit(1)
+		}
+		todos.edit(index,parts[1])
+	case cf.Toggle != -1: 	
+		todos.toggle(cf.Toggle)
+	case cf.Del != -1:
+		todos.delete(cf.Del)
+
+	default:
+		fmt.Println("Invalid command")	
 	}
+
+
 }
