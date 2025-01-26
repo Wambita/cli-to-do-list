e50cd969356f21f2d8e8aa43c49859dc0fbf454d
shfana <wambitafana@gmail.com>
2025-01-26 17:39:20 +0300

    Feat: create a add function that creates a todo struct and adds it to the list

--- todo.go
+++ todo.go
@@ -2,6 +2,7 @@ package main
 
 import "time"
 
+//to do struct
 type Todo struct {
 	Title       string
 	Completed   bool
@@ -9,7 +10,17 @@ type Todo struct {
 	CompletedAt *time.Time
 }
 
+//to do list
 type Todos []Todo
 
+//func add that creates a to do and adds it to the list
 func (todos *Todos) add(title string) {
+	todo := Todo{
+		Title:       title,
+		Completed:   false,
+		CompletedAt: nil,
+		CreatedAt:   time.Now(),
+	}
+	//append a todo to the  todo list
+	*todos = append(*todos, todo)
 }
